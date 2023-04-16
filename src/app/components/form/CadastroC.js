90dc1d425b03b4a7cb8ef72543fcf52b99bf2d5f
ValenteXD <jpcovello@hotmail.com>
2023-04-16 00:18:36 -0300

    Tela de cadastro corrigida

--- src/app/components/form/CadastroC.js
+++ src/app/components/form/CadastroC.js
@@ -3,6 +3,7 @@ import Input from "./Input";
 import Button from "./Button";
 import "./LoginC.css"
 import { Link } from 'react-router-dom';
+import Logo from "../Logo";
 
 const CadastroC = () => {
     return (
@@ -10,11 +11,12 @@ const CadastroC = () => {
      <div className="effect-background"></div>
       <div className="login">
         <form>
+          <Logo ID='login'/>
           <Input id="nomer" label="Nome:   " placeholder="Digite seu nome" />
           <Input id="email" label="Email:  " placeholder="Digite seu email"/>
           <Input id="senha" label="Senha   " placeholder="Crie sua senha"/>
           <Input id="senha2" label="Senha   " placeholder="Digite de novo"/>
-          <Button label="Cadastrar" />
+          <Link to = '/inicio'><Button label="Cadastrar" /></Link>
         </form>
       </div>
     </>  
